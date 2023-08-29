89736585e6931d2e04ce5503f719fdc91dfb2d37
juan barrios <juanpablobarriosgrijalba@gmail.com>
2023-08-29 10:02:57 -0500

    Grabar-Base-de-datos

--- controllers/events.js
+++ controllers/events.js
@@ -1,5 +1,5 @@
 const { response } = require("express");
-
+const Evento = require("../models/evento");
 const getEventos = (req, res = response) => {
   res.json({
     ok: true,
@@ -7,11 +7,24 @@ const getEventos = (req, res = response) => {
   });
 };
 
-const crearEvento = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "crearEventos",
-  });
+const crearEvento = async (req, res = response) => {
+  const evento = new Evento(req.body);
+
+  try {
+    evento.user = req.uid;
+
+    const eventoGuardado = await evento.save();
+    res.json({
+      ok: true,
+      evento: eventoGuardado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "hable con el administrador",
+    });
+  }
 };
 
 const actualizarEvento = (req, res = response) => {
--- routes/events.js
+++ routes/events.js
@@ -2,6 +2,9 @@
 // /api/events
 
 const { Router } = require("express");
+const{check}=require("express-validator")
+const {isDate}=require("../helpers/isDate")
+const{validarCampos}= require("../middlewares/validar-campos")
 const { validarJWT } = require("../middlewares/validar-jwt");
 const {
   getEventos,
@@ -16,7 +19,17 @@ router.use(validarJWT);
 router.get('/', getEventos);
 
 // crear eventos
-router.post('/', crearEvento);
+router.post(
+  '/',
+  
+   [
+    check("title","El titulo es obligatorio").not().isEmpty(),
+    check("start","fecha de inicio es obligatoria").custom(isDate),
+    check("end","fecha de finalizacion es obligatoria").custom(isDate),
+    validarCampos
+   ],
+    crearEvento
+   );
 
 // actualizar evento
 router.put('/:id', actualizarEvento);
